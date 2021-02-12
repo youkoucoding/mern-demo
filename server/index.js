44d1106620f98d7664552e317909111be15b00c2
k020c1357 <k020c1357@it-neec.jp>
2021-02-12 14:18:42 +0900

    connected to local MongoDB

--- server/index.js
+++ server/index.js
@@ -3,10 +3,28 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extented: true }));
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extented: true }));
 app.use(cors());
 
-// mongodb atlas
+// mongodb
+const CONNECTION_URL = 'mongodb://127.0.0.1:27017';
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`server is running at ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
